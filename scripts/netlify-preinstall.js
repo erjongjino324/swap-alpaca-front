20fa23a8b851effe9cc5629cf97054f45d433fc3
erjongjino324 <erjon.gjino324@gmail.com>
2021-12-16 19:23:12 -0500

    fix: preinstall

--- scripts/netlify-preinstall.js
+++ scripts/netlify-preinstall.js
@@ -7,17 +7,23 @@ const { spawnSync } = require('child_process')
 //   - Get creative with the preinstall script... :)
 
 // Only run this script on Netlify
+console.log('running preinstall script')
 if (process.env.NETLIFY === 'true') {
   // this is a default Netlify environment variable
   // Check if .npmrc already exists, if it does then do nothing (otherwise we create an infinite yarn loop)
+  console.log('we are on netlify')
   if (!fs.existsSync('.npmrc')) {
+    console.log('npmrc does not exist')
     // Create .npmrc
     fs.writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}\nlegacy-peer-deps=true\n`)
+    console.log('created npmrc')
     fs.chmodSync('.npmrc', 0o600)
+    console.log('chmod on npmrc complete')
     // Run yarn again, because the yarn process which is executing
     // this script won't pick up the .npmrc file we just created.
     // The original yarn process will continue after this second yarn process finishes,
     // and when it does it will report "success Already up-to-date."
-    // spawnSync('yarn', { stdio: 'inherit' })
+    spawnSync('npm', { stdio: 'inherit' })
+    console.log('spawned npm')
   }
 }
