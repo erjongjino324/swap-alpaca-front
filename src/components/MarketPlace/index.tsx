1f9d15ec73d20c5fba4e6c65a327c81bb3367e1b
erjongjino324 <erjon.gjino324@gmail.com>
2021-12-21 10:56:21 +1300

    fix: rename

--- src/components/MarketPlace/index.tsx
+++ src/components/MarketPlace/index.tsx
@@ -1,17 +1,16 @@
 import { RADIO_ADDRESS } from '@radioshackswap/sdk'
-import { useUSDCValue } from '../../hooks/useUSDCPrice'
-import { useActiveWeb3React } from '../../services/web3'
 import { tryParseAmount } from '../../functions/parse'
 import { useCurrency } from '../../hooks/Tokens'
+import { useUSDCValue } from '../../hooks/useUSDCPrice'
+import { useActiveWeb3React } from '../../services/web3'
 
 export default function MarketPlace(): JSX.Element | null {
   const { chainId } = useActiveWeb3React()
-  const RADIO_ADD = RADIO_ADDRESS[chainId]
-  const rawAmount = tryParseAmount('1', useCurrency(RADIO_ADD))
-  const ratioValue = useUSDCValue(rawAmount)?.toSignificant(4, { groupSeparator: ',' })
+  const rawAmount = tryParseAmount('1', useCurrency(RADIO_ADDRESS[chainId]))
+  const radioTokenUSDCPrice = useUSDCValue(rawAmount)?.toFixed(2)
   return (
     <div className="w-auto flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
-      <div className="px-3 py-2 text-primary text-bold">${ratioValue}</div>
+      <div className="px-3 py-2 text-primary text-bold">${radioTokenUSDCPrice}</div>
     </div>
   )
 }
