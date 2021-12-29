330f4fe6cb18417d4bdc95d59821de0d127ddf2e
erjongjino324 <erjon.gjino324@gmail.com>
2021-12-29 13:24:11 +1300

    fix: removed inline css

--- src/components/RadioWithShadow/index.tsx
+++ src/components/RadioWithShadow/index.tsx
@@ -14,12 +14,7 @@ const RadioWithShadow: FC<{ className?: string }> = ({ children, className }) =>
         <div className="absolute top-[-19px] left-[59px] right-[59px] h-[20px]">
           <Image src="/images/swap/radio-top.png" alt="top" layout="fill" />
         </div>
-        <div
-          className="absolute left-[18px] right-[18px] top-1 h-[56px] rounded-[55px]"
-          style={{
-            filter: 'blur(5px)',
-          }}
-        >
+        <div className="absolute left-[18px] right-[18px] top-1 h-[56px] rounded-[55px] blur-sm">
           <Image src="/images/swap/highlight.png" alt="top" layout="fill" />
         </div>
 
@@ -32,25 +27,12 @@ const RadioWithShadow: FC<{ className?: string }> = ({ children, className }) =>
         </div>
 
         <div className="relative p-[36px] bg-[#2F2528] rounded-[55px]">
-          <div
-            className="absolute left-[14px] w-[21px] top-[73px] h-[400px]"
-            style={{ backgroundColor: 'rgba(255, 255, 255, 0.12)', filter: 'blur(18px)' }}
-          />
-          <div
-            className="absolute right-[14px] w-[21px] top-[73px] h-[389px]"
-            style={{
-              backgroundColor: 'rgba(255, 255, 255, 0.16)',
-              filter: 'blur(18px)',
-              border: '1px solid rgba(0, 0, 0, 0.22)',
-            }}
-          />
+          <div className="absolute left-[14px] w-[21px] top-[73px] bottom-[36px] blur-lg bg-white bg-opacity-[0.12]" />
+          <div className="absolute right-[14px] w-[21px] top-[73px] bottom-[36px] blur-lg bg-white bg-opacity-[0.16]" />
           <div className="absolute left-[14px] w-[32.5px] bottom-[12.5px] h-[31px]" style={{ filter: 'blur(4px)' }}>
             <Image src="/images/swap/radio-corner.png" alt="left" layout="fill" />
           </div>
-          <div
-            className="absolute left-[47px] right-[45px] bottom-[21px] h-[15px]"
-            style={{ backgroundColor: 'rgba(255, 255, 255, 0.29)', filter: 'blur(18px)' }}
-          ></div>
+          <div className="absolute left-[47px] right-[45px] bottom-[21px] h-[15px] blur-lg bg-white bg-opacity-[0.29]"></div>
           <div className=" left-[38px] right-[39px] top-[34px] bottom-[36px]">{children}</div>
         </div>
       </div>
