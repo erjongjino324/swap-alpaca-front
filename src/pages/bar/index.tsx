21402c9ce443ae9a5f8a204cbfd9924ef9532be9
erjongjino324 <erjon.gjino324@gmail.com>
2022-01-07 11:46:02 +1300

    fix: changed token images

--- src/pages/bar/index.tsx
+++ src/pages/bar/index.tsx
@@ -396,7 +396,7 @@ export default function Stake() {
                   <div className="flex items-center space-x-4">
                     <Image
                       className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
-                      src="/images/tokens/xsushi-square.jpg"
+                      src="/images/tokens/s-radio-square.jpg"
                       alt="xSHACK"
                       width={64}
                       height={64}
@@ -420,7 +420,7 @@ export default function Stake() {
                   <div className="flex items-center ml-8 space-x-4 md:ml-0">
                     <Image
                       className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
-                      src="/images/tokens/sushi-square.jpg"
+                      src="/images/tokens/radio-square.jpg"
                       alt="SHACK"
                       width={64}
                       height={64}
